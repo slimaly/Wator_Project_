f1b82ef9eea22a68ae3a808136547d4164d1fe18
Alycia Slimani <alyciaslimani@yahoo.fr>
2024-01-05 14:52:35 +0100

    update

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 import random
+import time
+import os
 class SeaAnimal:
 
     def __init__(self,position,animal):
         self.x, self.y = position
         self.animal = animal
+        """parent class for my sea animals
+        """
 
 class Fish(SeaAnimal):
     def __init__(self, position):
         super().__init__(position,'fish')
 
     def fish_move (self, grid):
+        """_summary_
+
+        Args:
+            grid (_type_): this method has to move a fish object within my grid (step by step) 
+            but he can't move if there is a fish around him
+        """
     
         direction = random.choice(['north', 'south', 'east', 'west'])
         
@@ -29,7 +39,7 @@ class Fish(SeaAnimal):
             self.x, self.y = new_x, new_y
 
         
-        grid[self.x][self.y] = 'fish'
+        grid[self.x][self.y] = 1
 
         print(self.x,self.y)
 
@@ -39,6 +49,12 @@ class Shark(SeaAnimal):
         super().__init__(position, "shark")
 
     def shark_move(self, grid):
+        """_summary_
+
+        Args:
+            grid (_type_): this method has to move a shark within the grid but he has to check for a fish around him first
+            if there is a fish, he replaces it and else, he searches for an empty area
+        """
         new_x, new_y = self.x, self.y
         direction = random.choice(['north', 'south', 'east', 'west'])
 
@@ -63,7 +79,7 @@ class Shark(SeaAnimal):
             #position initiale:
             grid[self.x][self.y] = 0
             #nouvelle position + "fish" devient "shark"
-            grid[new_x][new_y] = 'shark' 
+            grid[new_x][new_y] = 2 
             #MAJ coordonées
             self.x, self.y = new_x, new_y
 
@@ -71,21 +87,22 @@ class Shark(SeaAnimal):
         else:
             #si pas de poisson:
             grid[self.x][self.y] = 0
-            grid[new_x][new_y] = 'shark'
+            grid[new_x][new_y] = 2
 
         print(self.x, self.y)
 
 
 
-grid = [[0,0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0,],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0]]
+grid = [[  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ],
+        [  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ,  0  ]]
 
 shark_list = []
 fish_list = []
@@ -109,12 +126,14 @@ fish_list.append(Fish((6,0)))
 
 #boucle des chrons
 for tour in range(10):
-        
-        for i in grid:
-          print(i)
-        print('tour', tour)
-        
-        requin.shark_move(grid)
-        
-        for fish in fish_list:     
-                fish.fish_move(grid)
+            
+    for i in grid:
+        print(i)
+    print('tour', tour)
+    
+    requin.shark_move(grid)
+    
+    for fish in fish_list:     
+            fish.fish_move(grid)
+    time.sleep(0.5)
+    os.system('cls')
